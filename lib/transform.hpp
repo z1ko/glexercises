15e0e57c4a34ec415273e144fac22d3f19450040
Z1ko <filippo.ziche@gmail.com>
2023-07-06 14:20:18 +0200

    section 10

--- lib/transform.hpp
+++ lib/transform.hpp
@@ -6,11 +6,14 @@
 
 namespace glib {
 
+const glm::vec3 UP(0.0f, 1.0f, 0.0f);
+
 struct camera_t {
-  glm::mat4 view;
 
   glm::vec3 position;
   glm::vec3 front;
+  glm::vec3 right;
+  glm::vec3 up;
 
   float yaw, pitch;
 };
@@ -18,7 +21,10 @@ struct camera_t {
 camera_t camera_base(const glm::vec3 &position);
 
 void camera_input(camera_t &camera, GLFWwindow *window, float delta, bool fps = false);
-void camera_look_at(camera_t &camera, const glm::vec3 &target);
+void camera_input_mouse(camera_t &camera, float xoffset, float yoffset);
+
+glm::mat4 camera_look_at(camera_t &camera, const glm::vec3 &target);
+glm::mat4 camera_view(const camera_t &camera);
 
 #ifdef GLIB_TRANSFORM_IMPL
 #undef GLIB_TRANSFORM_IMPL
@@ -26,10 +32,14 @@ void camera_look_at(camera_t &camera, const glm::vec3 &target);
 camera_t camera_base(const glm::vec3 &position) {
   camera_t result;
   result.position = position;
-  result.front = glm::vec3(0.0f, 0.0f, -1.0f);
-  result.yaw = 0.0f;
+  result.yaw = -90.0f;
   result.pitch = 0.0f;
-  result.view = glm::lookAt(result.position, result.position + result.front, glm::vec3(0.0f, 1.0f, 0.0f));
+
+  result.front = glm::vec3(0.0f, 0.0f, -1.0f);
+  result.right = glm::normalize(glm::cross(result.front, UP));
+  result.up    = glm::normalize(glm::cross(result.right, result.front));
+
+  //result.view = glm::lookAt(result.position, result.position + result.front, result.up);
   return result;
 }
 
@@ -46,47 +56,43 @@ void camera_input(camera_t &camera, GLFWwindow *window, float delta, bool fps) {
       camera.position -= glm::normalize(glm::cross(camera.front, cameraUp)) * cameraSpeed;
   if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
       camera.position += glm::normalize(glm::cross(camera.front, cameraUp)) * cameraSpeed;
+  if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+    camera_input_mouse(camera, -5.0f, 0.0f);
+  if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+    camera_input_mouse(camera,  5.0f, 0.0f);
+  if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    camera.position += cameraSpeed * camera.up;
+  if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+    camera.position -= cameraSpeed * camera.up;
 
-  // Mouse
-  /*
-  static double last_x, last_y;
-  static bool mouse_initialized = false;
-  if (!mouse_initialized) {
-    mouse_initialized = true;
-    glfwGetCursorPos(window, &last_x, &last_y); 
-  }
+  if (fps)
+    camera.position.y = 0.0f;
+  
+  //camera.view = glm::lookAt(camera.position, camera.position + camera.front, camera.up);
+}
 
-  double x, y;
-  glfwGetCursorPos(window, &x, &y); 
+void camera_input_mouse(camera_t &camera, float xoffset, float yoffset) {
+  const float SENSITIVITY = 0.01f;
 
-  float xoffset = x - last_x;
-  float yoffset = last_y - y;
-
-  const float cameraSensitivity = 0.1f;
-  camera.yaw   += xoffset * cameraSensitivity;
-  camera.pitch += yoffset * cameraSensitivity;
+  camera.yaw   += xoffset * SENSITIVITY;
+  camera.pitch += yoffset * SENSITIVITY;
 
   if (camera.pitch >  89.0f) camera.pitch =  89.0f;
   if (camera.pitch < -89.0f) camera.pitch = -89.0f;
 
-  // Calculate front of camera
-  glm::vec3 direction(
-    cos(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch)),
-    sin(glm::radians(camera.pitch)),
-    sin(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch))
-  );
-  
-  camera.front = glm::normalize(direction);
-  
-  */
+  glm::vec3 front;
+  front.x = cos(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
+  front.y = sin(glm::radians(camera.pitch));
+  front.z = sin(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
 
-  if (fps)
-    camera.position.y = 0.0f;
+  camera.front = glm::normalize(front);
+  camera.right = glm::normalize(glm::cross(camera.front, UP));
+  camera.up    = glm::normalize(glm::cross(camera.right, camera.front));
 
-  camera.view = glm::lookAt(camera.position, camera.position + camera.front, glm::vec3(0.0f, 1.0f, 0.0f));
+  //camera.view = glm::lookAt(camera.position, camera.position + camera.front, camera.up);
 }
 
-void camera_look_at(camera_t &camera, const glm::vec3 &target) {
+glm::mat4 camera_look_at(camera_t &camera, const glm::vec3 &target) {
 
   glm::vec3 zax = glm::normalize(camera.position - target);
   glm::vec3 xax = glm::normalize(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), zax));
@@ -106,8 +112,15 @@ void camera_look_at(camera_t &camera, const glm::vec3 &target) {
   rotation[1][2] = zax.y;
   rotation[2][2] = zax.z;
 
-  camera.view = rotation * translation;
   camera.front = glm::normalize(target - camera.position);
+  camera.right = glm::normalize(glm::cross(camera.front, UP));
+  camera.up    = glm::normalize(glm::cross(camera.right, camera.front));
+
+  return rotation * translation;
+}
+
+glm::mat4 camera_view(const camera_t &camera) {
+  return glm::lookAt(camera.position, camera.position + camera.front, camera.up);
 }
 
 #endif
